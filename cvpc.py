74acbb4f970cccabd35e94949db423654db9866e
root <root@ip-172-31-39-103.ap-south-1.compute.internal>
2020-10-22 09:49:00 +0000

    createvpc using jenkins and list instances using jenkins

---
+++ cvpc.py
@@ -0,0 +1,51 @@
+import boto3
+import sys
+
+region = sys.argv[1]
+access_key = sys.argv[2]
+secret_key= sys.argv[3]
+
+client = boto3.client('ec2',
+         region_name = region,
+         aws_access_key_id = access_key,
+         aws_secret_access_key = secret_key)
+myvpc = client.create_vpc(
+        CidrBlock='192.168.0.0/16',
+        InstanceTenancy='default',
+        TagSpecifications=[{'ResourceType': 'vpc','Tags': [{'Key': 'Name','Value': 'Ktexperts-VPC'},]},])
+print("My VPC ID :")
+print(myvpc['Vpc']['VpcId'])   
+
+mysubnet = client.create_subnet(
+           CidrBlock='192.168.1.0/24',
+	       VpcId=myvpc['Vpc']['VpcId'],
+           AvailabilityZone='ap-south-1a',
+           TagSpecifications=[{'ResourceType': 'subnet','Tags': [{'Key': 'Name','Value': 'MySubnet'},]},])
+print("My Subnet ID :")
+print(mysubnet['Subnet']['SubnetId'])
+
+myroutetable=client.create_route_table(
+             VpcId=myvpc['Vpc']['VpcId'],
+			 TagSpecifications=[{'ResourceType': 'route-table','Tags': [{'Key': 'Name','Value': 'Ktexpers-RT'},]},])
+print("My Route Table ID :")
+print(myroutetable['RouteTable']['RouteTableId'])
+
+myigw=client.create_internet_gateway(
+      TagSpecifications=[{'ResourceType': 'internet-gateway', 'Tags': [{'Key': 'Name','Value': 'Ktexperts-IGW'},]},])
+print("My Internet Gateway :")
+print(myigw['InternetGateway']['InternetGatewayId'])
+
+Mysubnetassociate=client.associate_route_table(
+                  RouteTableId=myroutetable['RouteTable']['RouteTableId'],
+                  SubnetId=mysubnet['Subnet']['SubnetId'])
+print("Subnet Associated")
+
+Myigwattach=client.attach_internet_gateway(
+            InternetGatewayId=myigw['InternetGateway']['InternetGatewayId'],
+            VpcId=myvpc['Vpc']['VpcId'])
+
+Myrtentry = client.create_route(
+            DestinationCidrBlock='0.0.0.0/0',
+	        RouteTableId=myroutetable['RouteTable']['RouteTableId'],
+            GatewayId=myigw['InternetGateway']['InternetGatewayId'])
+print("Route entry entered")
